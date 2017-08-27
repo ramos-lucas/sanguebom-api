a8bf9ee01a58541c86ff57a2de8b2de47a02c4e4
Lucas Ramos <ramos.lucash@gmail.com>
2017-08-27 20:51:00 -0300

    add CRUD REST - basic (v10)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -9,6 +13,24 @@ const route = router.get('/', (req, res, next) => {
         version: "0.0.1"
     });
 });
+
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({ 
+        id: id, 
+        item: req.body
+    });
+});
+const del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
 app.use('/', route);
+app.use('/usuarios', create);
+app.use('/usuarios', put);
+app.use('/usuarios', del);
 
 module.exports = app;

d2d5ec204e8fab229485fe202e7e6a83314c650a
Lucas Ramos <ramos.lucash@gmail.com>
2017-08-27 21:40:39 -0300

    add ROUTES usuario - basic (v11)

--- src/app.js
+++ src/app.js
@@ -4,33 +4,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+// carrega as rotas
+const indexRoute = require('./routes/index-route');
+const usuarioRoute = require('./routes/usuario-route');
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ 
+    extended: false 
+}));
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node server",
-        version: "0.0.1"
-    });
-});
-
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({ 
-        id: id, 
-        item: req.body
-    });
-});
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-app.use('/', route);
-app.use('/usuarios', create);
-app.use('/usuarios', put);
-app.use('/usuarios', del);
+app.use('/', indexRoute);
+app.use('/usuarios', usuarioRoute);
 
 module.exports = app;

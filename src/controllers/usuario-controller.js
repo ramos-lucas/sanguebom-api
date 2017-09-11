4b14dabc05f51335f3dcbb13f60a872906816934
Lucas Ramos <ramos.lucash@gmail.com>
2017-09-11 17:35:01 -0300

    add async and await

--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -3,40 +3,43 @@ const Usuario = mongoose.model('Usuario');
 const Validacao = require('../validators/validacao');
 const repository = require('../repositories/usuario-repository');
 
-exports.get = (req, res, next) => {
-    repository
-        .get()
-        .then(data => {
-            res.status(200).send(data);
-        })
-        .catch(e => {
-            res.status(400).send(e);
-        })
+exports.get = async(req, res, next) => {
+    try {
+        var data = await repository.get();
+        res.status(200).send(data);
+    } catch(e) {
+        res.status(400).send({
+            message: 'Falha ao processar sua requisição',
+            data: e
+        });
+    }
 };
 
-exports.getByUsername = (req, res, next) => {
-    repository
-        .getByUsername(req.params.username)
-        .then(data => {
-            res.status(200).send(data);
-        })
-        .catch(e => {
-            res.status(400).send(e);
-        })
+exports.getByUsername = async(req, res, next) => {
+    try{
+        var data = await repository.getByUsername(req.params.username);
+        res.status(200).send(data);
+    } catch(e) {
+        res.status(400).send({
+            message: 'Falha ao processar sua requisição',
+            data: e
+        });
+    }
 };
 
-exports.getById = (req, res, next) => {
-    repository
-        .getById(req.params.id)
-        .then(data => {
-            res.status(200).send(data);
-        })
-        .catch(e => {
-            res.status(400).send(e);
-        })
+exports.getById = async(req, res, next) => {
+    try{
+        var data = await repository.getById(req.params.id);
+        res.status(200).send(data);
+    } catch(e) {
+        res.status(400).send({
+            message: 'Falha ao processar sua requisição',
+            data: e
+        });
+    }
 };
 
-exports.post = (req, res, next) => {
+exports.post = async(req, res, next) => {
     let contrato = new Validacao();
     contrato.usernameFormat(req.body.username, 'O nome de usuário deve conter de 5 a 20 caracteres, sendo letras (excluindo acentos), números e underline.');
 
@@ -45,49 +48,43 @@ exports.post = (req, res, next) => {
         return;
     }
 
-    repository
-        .create(req.body)
-        .then(x => {
-            res.status(201).send({ 
-                message: 'Usuario cadastrado com sucesso!'
-            });
-        })
-        .catch(e => {
-            res.status(400).send({ 
-                message: 'Falha ao cadastrar o usuário!',
-                data: e
-            });
+    try{
+        await repository.create(req.body);
+        res.status(201).send({
+            message: 'Usuario cadastrado com sucesso!'
         });
+    } catch(e) {
+        res.status(400).send({
+            message: 'Falha ao cadastrar o usuário!',
+            data: e
+        });
+    }
 };
 
-exports.put = (req, res, next) => {
-    repository
-        .update(req.params.id, req.body)
-        .then(x => {
-            res.status(200).send({
-                message: 'Usuario atualizado com sucesso!'
-            });
-        })
-        .catch(e => {
-            res.status(400).send({
-                message: 'Falha ao atualizar o usuário',
-                data: e
-            });
+exports.put = async(req, res, next) => {
+    try{
+        await repository.update(req.params.id, req.body);
+        res.status(200).send({
+            message: 'Usuario atualizado com sucesso!'
         });
+    } catch(e) {
+        res.status(400).send({
+            message: 'Falha ao atualizar o usuário',
+            data: e
+        });
+    }
 };
 
-exports.delete = (req, res, next) => {
-    repository
-        .delete(req.params.id)
-        .then(x => {
-            res.status(200).send({
-                message: 'Usuario removido com sucesso!'
-            });
-        })
-        .catch(e => {
-            res.status(400).send({
-                message: 'Falha ao remover o usuário',
-                data: e
-            });
+exports.delete = async(req, res, next) => {
+    try{
+        await repository.delete(req.params.id);
+        res.status(200).send({
+            message: 'Usuario removido com sucesso!'
         });
+    } catch(e) {
+        res.status(400).send({
+            message: 'Falha ao remover o usuário',
+            data: e
+        });
+    }
 };
--- src/repositories/usuario-repository.js
+++ src/repositories/usuario-repository.js
@@ -1,49 +1,51 @@
 const mongoose = require('mongoose');
 const Usuario = mongoose.model('Usuario');
 
-exports.get = () => {
-    return Usuario
-        .find({});
+exports.get = async() => {
+    const res = await Usuario.find({});
+    return res;
 }
 
-exports.getByUsername = (username) => {
-    return Usuario
+exports.getByUsername = async(username) => {
+    const res = await Usuario
         .findOne({
             username: username
         });
+    return res;
 }
 
-exports.getById = (id) => {
-    return Usuario
+exports.getById = async(id) => {
+    const res = await Usuario
         .findById(id);
+    return res;
 }
 
-exports.create = (data) => {
+exports.create = async(data) => {
     var usuario = new Usuario(data);
-    return usuario.save()
+    await usuario.save()
 }
 
-exports.update = (id, data) => {
-    return Usuario
-    .findByIdAndUpdate(id, {
-        $set: {
-            'nome': data.nome,
-            'username': data.username,
-            'senha': data.senha,
-            'cpf': data.cpf,
-            'email': data.email,
-            'telefone': data.telefone,
-            'dtNascimento': data.dtNascimento,
-            'avatar': data.avatar,
-            'sangue': {
-                'tipo': data.sangue.tipo,
-                'fator': data.sangue.fator
+exports.update = async(id, data) => {
+    await Usuario
+        .findByIdAndUpdate(id, {
+            $set: {
+                'nome': data.nome,
+                'username': data.username,
+                'senha': data.senha,
+                'cpf': data.cpf,
+                'email': data.email,
+                'telefone': data.telefone,
+                'dtNascimento': data.dtNascimento,
+                'avatar': data.avatar,
+                'sangue': {
+                    'tipo': data.sangue.tipo,
+                    'fator': data.sangue.fator
+                }
             }
-        }
-    });
+        });
 }
 
-exports.delete = (id) => {
-    return Usuario
+exports.delete = async(id) => {
+    await Usuario
         .findOneAndRemove(id);
 }
